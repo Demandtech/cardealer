77f9514597ef3c6c09b5fd7eccf2779cdb87313b
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2023-01-05 15:35:53 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+let linkContainer = document.querySelector('.nav-list');
+let links = linkContainer.getElementsByClassName('nav-link');
+let navClose = document.querySelector('.menu-close')
+let navOpen = document.querySelector('.menu-open');
+let body = document.querySelector('body')
+
+
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener('click', function () {
+    let current = document.getElementsByClassName('nav-active')
+    current[0].className = current[0].className.replace(' nav-active', '')
+    this.className += ' nav-active'
+  })
+}
+
+navOpen.addEventListener('click', ()=> {
+  console.log('clicked');
+  linkContainer.classList.add('nav-open');
+  navOpen.style.opacity = 0;
+  body.style.position = 'fixed';
+})
+
+navClose.addEventListener('click', () => {
+  console.log('clicked')
+  linkContainer.classList.remove('nav-open')
+  navOpen.style.opacity = 1;
+  body.style.position = 'static'
+})
